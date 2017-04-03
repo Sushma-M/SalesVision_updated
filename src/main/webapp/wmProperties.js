667ecafe4597b9d2bf78bc6feaa61a661d45b776
root <root@c1fd970146ea>
2017-04-03 05:17:50 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "flyer",
+  "defaultLanguage" : "en",
+  "displayName" : "SalesVision_updated",
+  "homePage" : "Main",
+  "name" : "SalesVision_updated",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
